60960c0563f0a58f2d63cb5f36d338fcc4ee45e2
PoN1EXE <PoN1EXE1@gmail.com>
2026-07-16 03:39:42 +0300

    инпут готовый с фильтрацией

--- src/components/CoinList/CoinList.tsx
+++ src/components/CoinList/CoinList.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import { useCrypto } from '../../hooks/useCrypto'
 import styles from './CoinList.module.scss'
 
 export const CoinList = () => {
   const { data: coins, isLoading, isError, error } = useCrypto()
+  const [search, setSearch] = useState('')
 
   if (isLoading) {
     return <div className={styles.loading}>Загрузка...</div>
@@ -12,28 +14,35 @@ export const CoinList = () => {
     return <div className={styles.error}>Ошибка: {error.message}</div>
   }
 
+  const filteredData = coins?.filter((coins) => coins.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Криптовалюты</h2>
-      <ul className={styles.list}>
-        {coins?.map((coin) => (
-          <li key={coin.id} className={styles.item}>
-            <div className={styles.left}>
-              <span className={styles.icon}>{coin.icon}</span>
-              <span className={styles.name}>{coin.name}</span>
-              <span className={styles.symbol}>{coin.symbol}</span>
-            </div>
-            <div className={styles.right}>
-              <span className={styles.price}>${coin.price.toFixed(2)}</span>
-              <span className={`${styles.change} ${coin.change24h >= 0 ? styles.positive : styles.negative}`}>
-                {coin.change24h > 0 ? '+' : ''}
-                {coin.change24h.toFixed(2)}%
-              </span>
-              <span className={styles.volume}>Vol: {coin.volume.toLocaleString()}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <input type='text' value={search} onChange={(e) => setSearch(e.target.value)} placeholder='Поиск...' />
+      {filteredData?.length === 0 ? (
+        <div>'Ничего не найдено...'</div>
+      ) : (
+        <ul className={styles.list}>
+          {filteredData?.map((coin) => (
+            <li key={coin.id} className={styles.item}>
+              <div className={styles.left}>
+                <span className={styles.icon}>{coin.icon}</span>
+                <span className={styles.name}>{coin.name}</span>
+                <span className={styles.symbol}>{coin.symbol}</span>
+              </div>
+              <div className={styles.right}>
+                <span className={styles.price}>${coin.price.toFixed(2)}</span>
+                <span className={`${styles.change} ${coin.change24h >= 0 ? styles.positive : styles.negative}`}>
+                  {coin.change24h > 0 ? '+' : ''}
+                  {coin.change24h.toFixed(2)}%
+                </span>
+                <span className={styles.volume}>Vol: {coin.volume.toLocaleString()}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
